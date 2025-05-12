d78edec616184009131160da474d7b361b785368
John Moorman <45500240+mojoro@users.noreply.github.com>
2025-05-12 23:28:02 +0200

    Reach incorrect solution for day 8 pt 2

--- day-8-puzzle-pt2.py
+++ day-8-puzzle-pt2.py
@@ -2,42 +2,64 @@ class CamelMap:
   def __init__(self, map):
     self.left = map.split('(')[1].split(', ')[0]
     self.right = map.split('(')[1].split(', ')[1][:3]
+    self.key = map.split(' = ')[0]
 
 def constructGraph(maps):
-
   graph = {}
   for map in maps:
     graph[map.split(' = ')[0]] = CamelMap(map)
 
   return graph
 
-def navigateToEnd(instructions, graph):
+def navigateToEnd(instructions, graph, aMaps, zMaps):
   steps = 0
-  origin = 'AAA'
-  currentLocation = origin
-  destination = 'ZZZ'
+  currentMaps = aMaps
+  destinationSet = set(zMaps)
   destinationReached = False
   directions = list(instructions)
   while not destinationReached:
     for direction in directions:
+      nextMaps = []
       if direction == 'L':
-        currentLocation = graph[currentLocation].left
+        for map in currentMaps:
+          nextMaps.append(graph[map].left)
+        currentMaps = nextMaps
         steps += 1
       elif direction == 'R':
-        currentLocation = graph[currentLocation].right
+        for map in currentMaps:
+          nextMaps.append(graph[map].right)
+        currentMaps = nextMaps
         steps += 1
-      if currentLocation == destination:
+      if set(currentMaps) == destinationSet:
         destinationReached = True
         break
   return steps
 
+def returnAMaps(maps):
+  aMaps = []
+  for map in maps:
+    if map[2] == 'A':
+      aMaps.append(CamelMap(map).key)
+  
+  return aMaps
 
-
+def returnZMaps(maps):
+  zMaps = []
+  for map in maps:
+    if map[2] == 'Z':
+      zMaps.append(CamelMap(map).key)
+  
+  return zMaps
 
 with open('day-8-input.txt') as f:
   lines = [line for line in f.read().splitlines()]
   instructions = lines[0]
   maps = lines[2:]
   graph = constructGraph(maps)
-  steps = navigateToEnd(instructions, graph)
+  aMaps = returnAMaps(maps)
+  zMaps = returnZMaps(maps)
+  steps = navigateToEnd(instructions, graph, aMaps, zMaps)
   print(steps)
+
+  # collect all things ending with A. while they don't all end in Z, Navigate all according to their direction, counting steps.
+  # evaluate if they all end in Z
